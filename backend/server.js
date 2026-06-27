e8e4614c4b85c9aeebd39c09a47e5037aae54bce
Rusil Koirala <rusilkoirala23@gmail.com>
2026-06-27 22:17:49 +0545

    feat: prod issue fixing

--- backend/server.js
+++ backend/server.js
@@ -1,55 +1,65 @@
-import express, { json } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import db from "./src/lib/db.js";
-import { sql } from "drizzle-orm";
-import 'dotenv/config';
-import monitorRoutes from "./src/routes/monitor.js";
-import { startSchedular } from "./src/services/checker.js";
-import userRoutes from "./src/routes/user.js";
-import teamRoutes from "./src/routes/team.js";
-import statusPageRoutes from "./src/routes/statusPage.js";
+import express, { json } from "express"
+import cors from "cors"
+import db from "./src/lib/db.js"
+import { sql } from "drizzle-orm"
+import 'dotenv/config' 
+import monitorRoutes from "./src/routes/monitor.js"
+import { startSchedular } from "./src/services/checker.js"
+import userRoutes from "./src/routes/user.js"
+import teamRoutes from "./src/routes/team.js"
+import statusPageRoutes from "./src/routes/statusPage.js"
+ import cookieParser from "cookie-parser"
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(express.json())
+app.use(cookieParser())
+
+
 
 // Cors setting
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://pulseops.rusil.me",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
-}));
+}))
 
-// Health route
-app.get("/healthz", async (req, res) => {
-  try {
-    await db.execute(sql`SELECT 1`);
-    res.json({ success: true, message: "Server is healthy :)" });
-  } catch (error) {
-    res.status(500).json({ success: false, message: "Server is unhealthy :(", error: error.message });
-  }
-});
 
-app.use("/api/monitors", monitorRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/teams", teamRoutes);
-app.use("/api/status-pages", statusPageRoutes);
+// Heatlth route
+app.get("/healthz", async (req,res)=> {
+    try {
+        await db.execute(sql`SELECT 1`)
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
-  console.log(`Server is running at port http://localhost:${PORT}`);
-  await startSchedular();
-});
+        res.json({
+            success: true,
+            message: "Server is healthy :)"
+        })
+    }
+    catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Server is unhealthy :(",
+            error: error.message
+        })
+    }
+})
+
+
+app.use("/api/monitors", monitorRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/teams",teamRoutes)
+app.use("/api/status-pages", statusPageRoutes)
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,  async()=>{
+    console.log(`Server is running at port http://localhost:${PORT}`)
+    await startSchedular()
+})
+
